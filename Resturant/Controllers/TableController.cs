86cab047deabdff266dde26a5c202915fbef181a
A_Sharaawy <abdallahsharawy200@gmail.com>
2025-07-29 18:05:18 +0300

    add update   and create form

--- Resturant/Controllers/TableController.cs
+++ Resturant/Controllers/TableController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Resturant_BLL.DTOModels;
 using Resturant_BLL.Services;
 
 namespace Resturant_PL.Controllers
@@ -15,6 +16,15 @@ namespace Resturant_PL.Controllers
         public IActionResult Index()
         {
             return View("Tables",_TS.GetList());
+        }
+        public IActionResult Update(int id)
+        {
+            TableDTO table = _TS.GetById(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+            return View("Update",table);
         }
     }
 }
